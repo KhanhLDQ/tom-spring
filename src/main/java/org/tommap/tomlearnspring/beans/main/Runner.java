58e39298432784815bb5e0b3b366f559232e9609
Tom <tommapcoder@gmail.com>
2024-12-22 10:46:57 +0700

    creating beans programmatically

--- src/main/java/org/tommap/tomlearnspring/beans/main/Runner.java
+++ src/main/java/org/tommap/tomlearnspring/beans/main/Runner.java
@@ -1,9 +1,14 @@
 package org.tommap.tomlearnspring.beans.main;
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.tommap.tomlearnspring.beans.config.ProjectConfiguration;
 import org.tommap.tomlearnspring.beans.model.Animal;
+import org.tommap.tomlearnspring.beans.model.Company;
 import org.tommap.tomlearnspring.beans.model.Vehicle;
+
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class Runner {
     public static void main(String[] args) {
@@ -15,6 +20,53 @@ public class Runner {
 
         Animal animal = context.getBean(Animal.class);
         animal.sayHello();
+
+        //creating beans programmatically
+        Supplier<Company> saigonTechSupplier = () -> {
+            var saigonTech = new Company();
+            saigonTech.setName("SaigonTech");
+
+            return saigonTech;
+        };
+
+        Supplier<Company> tamaraSupplier = () -> {
+            var tamara = new Company();
+            tamara.setName("Tamara");
+
+            return tamara;
+        };
+
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+        System.out.println("Random number: " + randomNum);
+
+        if (0 == (randomNum % 2)) {
+            context.registerBean("saigonTech", Company.class, saigonTechSupplier);
+        } else {
+            context.registerBean("tamara", Company.class, tamaraSupplier);
+        }
+
+        Company saigonTech = null, tamara = null;
+
+        try {
+            saigonTech = context.getBean("saigonTech", Company.class);
+        } catch (NoSuchBeanDefinitionException ex) {
+            System.out.println("Error while creating SaigonTech bean: " + ex.getMessage());
+        }
+
+        try {
+            tamara = context.getBean("tamara", Company.class);
+        } catch (NoSuchBeanDefinitionException ex) {
+            System.out.println("Error while creating Tamara bean: " + ex.getMessage());
+        }
+
+        if (null != saigonTech) {
+            saigonTech.sayHello();
+        } else if (null != tamara){
+            tamara.sayHello();
+        } else {
+            System.out.println("neither SaigonTech nor Tamara bean is created");
+        }
 
         context.close();
     }
---
+++ src/main/java/org/tommap/tomlearnspring/beans/model/Company.java
@@ -0,0 +1,17 @@
+package org.tommap.tomlearnspring.beans.model;
+
+public class Company {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Company name from Spring context: " + name);
+    }
+}
