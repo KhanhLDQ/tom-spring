657e0aa5a460e113a052090e0a62327bf1ecae23
Tom <tommapcoder@gmail.com>
2025-02-20 23:48:17 +0700

    build courses page

---
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/config/WebConfig.java
@@ -0,0 +1,14 @@
+package org.tommap.tomlearnspring.eazy_school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //create a direct mapping between the URL and view name without creating a separate controller
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/courses").setViewName("courses");
+    }
+}
