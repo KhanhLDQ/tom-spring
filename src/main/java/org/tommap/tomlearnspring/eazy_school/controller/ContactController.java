91b3d3a4b304dc71f5c794c6f75803a0acd9de41
Tom <tommapcoder@gmail.com>
2025-02-21 11:02:27 +0700

    convert request param to pojo object

--- src/main/java/org/tommap/tomlearnspring/eazy_school/controller/ContactController.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/controller/ContactController.java
@@ -1,17 +1,22 @@
 package org.tommap.tomlearnspring.eazy_school.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.tommap.tomlearnspring.eazy_school.model.Contact;
+import org.tommap.tomlearnspring.eazy_school.service.IContactService;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
-    private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
+    private final IContactService contactService;
+
+    @Autowired
+    public ContactController(IContactService contactService) {
+        this.contactService = contactService;
+    }
 
     @RequestMapping("/contact")
     public String displayContactPage() {
@@ -20,18 +25,9 @@ public class ContactController {
 
     @RequestMapping(path = "/saveMsg", method = POST)
     public ModelAndView saveMsg(
-            @RequestParam(name = "name") String name,
-            @RequestParam(name = "mobileNum") String mobileNumber,
-            @RequestParam(name = "email") String email,
-            @RequestParam(name = "subject") String subject,
-            @RequestParam(name = "message") String message
+            Contact contact
     ) {
-        logger.info("Name: " + name);
-        logger.info("Mobile Number: " + mobileNumber);
-        logger.info("Email: " + email);
-        logger.info("Subject: " + subject);
-        logger.info("Message: " + message);
-
+        contactService.saveMsgDetails(contact);
         return new ModelAndView("redirect:/contact"); //club model data and view information
     }
 }
---
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/service/IContactService.java
@@ -0,0 +1,7 @@
+package org.tommap.tomlearnspring.eazy_school.service;
+
+import org.tommap.tomlearnspring.eazy_school.model.Contact;
+
+public interface IContactService {
+    boolean saveMsgDetails(Contact contact);
+}
