bff6d1af23c783747c7c63018f8e3cbb96f6200d
Tom <tommapcoder@gmail.com>
2025-02-24 11:26:23 +0700

    lombok library

--- src/main/java/org/tommap/tomlearnspring/eazy_school/controller/HolidayController.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/controller/HolidayController.java
@@ -7,7 +7,6 @@ import org.tommap.tomlearnspring.eazy_school.model.Holiday;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class HolidayController {
@@ -30,7 +29,7 @@ public class HolidayController {
                     type.toString(),
                     holidays.stream()
                             .filter(holiday -> holiday.getType().equals(type))
-                            .collect(Collectors.toList())
+                            .toList()
             );
         }
 
--- src/main/java/org/tommap/tomlearnspring/eazy_school/service/ContactServiceImpl.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/service/ContactServiceImpl.java
@@ -1,19 +1,16 @@
 package org.tommap.tomlearnspring.eazy_school.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.tommap.tomlearnspring.eazy_school.model.Contact;
 
 @Service
+@Slf4j
 public class ContactServiceImpl implements IContactService {
-    private static final Logger logger = LoggerFactory.getLogger(ContactServiceImpl.class);
-
     @Override
     public boolean saveMsgDetails(Contact contact) {
         boolean isSaved = false;
-        //TODO: need to persist the data into the database table
-        logger.info(contact.toString());
+        log.info(contact.toString());
 
         return isSaved;
     }
