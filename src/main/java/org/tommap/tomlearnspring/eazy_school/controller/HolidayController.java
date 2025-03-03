279694e0cca96c9aee95b16067f73481924f560b
Tom <tommapcoder@gmail.com>
2025-03-03 23:14:15 +0700

    path variables

--- src/main/java/org/tommap/tomlearnspring/eazy_school/controller/HolidayController.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/controller/HolidayController.java
@@ -3,7 +3,7 @@ package org.tommap.tomlearnspring.eazy_school.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.tommap.tomlearnspring.eazy_school.model.Holiday;
 
 import java.util.Arrays;
@@ -11,14 +11,27 @@ import java.util.List;
 
 @Controller
 public class HolidayController {
-    @GetMapping("/holidays")
+    @GetMapping("/holidays/{display}")
     public String displayHolidays(
-            @RequestParam(required = false) boolean festival,
-            @RequestParam(required = false) boolean federal,
+            @PathVariable String display,
             Model model
     ) {
-        model.addAttribute("is_show_festival", festival);
-        model.addAttribute("is_show_federal", federal);
+        boolean is_show_festival = false;
+        boolean is_show_federal = false;
+
+        if (null != display) {
+            switch (display) {
+                case "all" -> {
+                    is_show_festival = true;
+                    is_show_federal = true;
+                }
+                case "festival" -> is_show_festival = true;
+                case "federal" -> is_show_federal = true;
+            }
+        }
+
+        model.addAttribute("is_show_festival", is_show_festival);
+        model.addAttribute("is_show_federal", is_show_federal);
 
         List<Holiday> holidays = Arrays.asList(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
