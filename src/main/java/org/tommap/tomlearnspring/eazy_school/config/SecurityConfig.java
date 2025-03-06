408a974be98199cdabe7bd75bcd071037c4c183e
Tom <tommapcoder@gmail.com>
2025-03-06 10:58:04 +0700

    in-memory authentication

--- src/main/java/org/tommap/tomlearnspring/eazy_school/config/SecurityConfig.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +29,16 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("123456").roles("ADMIN", "USER").build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("123456").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
