b1331dfc540819833a19476c48bc69575f2d52f2
Khanh Le <tommapcoder@gmail.com>
2025-05-13 10:04:16 +0700

    read properties using Environment interface

--- src/main/java/org/tommap/tomlearnspring/eazy_school/rest/PropertiesDemoRestController.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/rest/PropertiesDemoRestController.java
@@ -1,7 +1,9 @@
 package org.tommap.tomlearnspring.eazy_school.rest;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/v1/api/rest-properties")
+@RequiredArgsConstructor
 @Slf4j
 public class PropertiesDemoRestController {
     @Value("${tom.pageSize}")
@@ -17,9 +20,18 @@ public class PropertiesDemoRestController {
     @Value("${tom.contact.successMsg}")
     private String successMsg;
 
+    private final Environment environment;
+
     @GetMapping("/read")
     public ResponseEntity<String> read() {
-        String properties = String.format("pageSize: %d, successMsg: %s", pageSize, successMsg);
+//        String properties = String.format("pageSize: %d, successMsg: %s", pageSize, successMsg);
+        String properties = String.format("pageSize: %d, successMsg: %s, javaHome: %s, javaVersion: %s",
+                environment.getProperty("tom.pageSize", Integer.class),
+                environment.getProperty("tom.contact.successMsg", String.class),
+                environment.getProperty("java.home", String.class),
+                environment.getProperty("java.version", String.class)
+        );
+
         return ResponseEntity.ok(properties);
     }
 }
