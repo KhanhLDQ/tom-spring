b79f79d76c878eb2ea03dcc3ed9edb8f10bd9c0f
Khanh Le <tommapcoder@gmail.com>
2025-05-14 07:55:38 +0700

    spring profiles

--- src/main/java/org/tommap/tomlearnspring/eazy_school/security/EazySchoolNonProdAuthenticationProvider.java
+++ src/main/java/org/tommap/tomlearnspring/eazy_school/security/EazySchoolNonProdAuthenticationProvider.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.tommap.tomlearnspring.eazy_school.model.Roles;
 import org.tommap.tomlearnspring.eazy_school.repository.PersonRepository;
@@ -19,24 +18,17 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-@Profile("prod")
-public class EazySchoolAuthenticationProvider implements AuthenticationProvider {
+@Profile("!prod")
+public class EazySchoolNonProdAuthenticationProvider implements AuthenticationProvider {
     private final PersonRepository personRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String email = authentication.getName();
-        String pwd = authentication.getCredentials().toString();
-
         var person = personRepository.findByEmail(email)
                 .orElseThrow(() -> new BadCredentialsException("User not found"));
 
-        if (passwordEncoder.matches(pwd, person.getPwd())) {
-            return new UsernamePasswordAuthenticationToken(email, null, getGrantedAuthorities(person.getRoles()));
-        } else {
-            throw new BadCredentialsException("Invalid credentials");
-        }
+        return new UsernamePasswordAuthenticationToken(email, null, getGrantedAuthorities(person.getRoles()));
     }
 
     @Override
